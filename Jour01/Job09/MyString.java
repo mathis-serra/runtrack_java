fdc85e51ccd3ee453bdfa63bede6fa8bd54e699e
mathis-serra <mathis.serra@laplateforme.io>
2024-06-10 22:40:41 +0200

    finish Job09

---
+++ Jour01/Job09/MyString.java
@@ -0,0 +1,26 @@
+
+
+public class MyString {
+    
+    public static void main(String[] args) {
+       
+        String str1 = "Bonjour";
+
+        
+        String str2 = new String("Hello");
+
+       
+        String baseString = "Bienvenue dans le monde Java";
+        String str3 = baseString.substring(0, 9);
+
+        
+        String str4;
+        str4 = "Salut";
+
+      
+        System.out.println("str1: " + str1);
+        System.out.println("str2: " + str2);
+        System.out.println("str3: " + str3);
+        System.out.println("str4: " + str4);
+    }
+}
