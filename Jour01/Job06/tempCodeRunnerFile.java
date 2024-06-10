02c19148656593d8ad7a4c31b0d1e56f12fb8004
mathis-serra <mathis.serra@laplateforme.io>
2024-06-10 22:15:26 +0200

    finish Job06

---
+++ Jour01/Job06/tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+
+
+public class ExerciceVariables {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        System.out.println("La valeur de num1 est " + num1 + " et la valeur de num2 est " + num2);
+    }
+}
