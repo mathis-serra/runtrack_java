334e87eccc2b116ed0657f0cce41fb51e254f21a
mathis-serra <mathis.serra@laplateforme.io>
2024-06-10 13:20:39 +0200

    finish Job03

---
+++ Jour01/Job03/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
+// The class name must be the same as the file name, so the file name must be HelloWorld.java
+/*
+    * The class name is the name of the file, so the file name must be HelloWorld.java
+    * The class name is the name of the file, so the file name must be HelloWorld.java
+    * The class name is the name of the file, so the file name must be HelloWorld.java
+*/
---
+++ Jour01/Job04/DeclarationVariable.java
@@ -0,0 +1,11 @@
+public class DeclarationVariable {
+    public static void main(String[] args) {   
+        int x, w, z;
+        x = w = z = 0;   
+
+        System.out.println(x);
+        System.out.println(w);
+        System.out.println(z);
+
+    }
+}
