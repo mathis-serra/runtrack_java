f4d88b96b472f081e097aa8c4b18286bcf4cd48a
mathis-serra <mathis.serra@laplateforme.io>
2024-06-12 10:27:10 +0200

    finish Job01

---
+++ Jour03/Job01/Tableau.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Tableau {
+    public static void main(String[] args) {
+        int[] tab = {10, 1, 2, 4, 69, 6};
+        System.out.println(Arrays.toString(tab));
+        System.out.println(tab[1]);
+    }
+}
