17a9154f94f1e0d996cf5a75396d62b6ffc42cc2
mathis-serra <mathis.serra@laplateforme.io>
2024-06-12 10:41:29 +0200

    finish Job03

---
+++ Jour03/Job03/Nom.java
@@ -0,0 +1,9 @@
+public class Nom {
+    public static void main(String[] args) {
+        String[] noms = {"Josette", "John", "Myrtille", "Marc"};
+        System.out.println(noms[1]);
+
+        noms[2] = "Mireille";
+        System.out.println(noms[2]);
+    }
+}
